4df0f1f0863d89085bc47f91eeecfed30e8b5a5e
Egor <egorlobzov8@gmail.com>
2023-05-20 21:40:07 +0300

    add Touka from tokyo ghoul

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp2.Model;
 using WpfApp2.Views;
 
 namespace WpfApp2
@@ -24,6 +25,7 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
+            AppData.CurrentUser = null;
             MainFrame.Navigate(new SignInPage());
         }
 
@@ -38,11 +40,52 @@ namespace WpfApp2
                 BtnGoBack.Visibility = Visibility.Hidden;
             }
 
+
+            if (AppData.CurrentUser != null)
+            {
+                MainMenu.Visibility = Visibility.Visible;
+            }
+
         }
 
         private void BtnGoBack_Click(object sender, RoutedEventArgs e)
         {
             MainFrame.GoBack();
+        }
+
+        private void OpenPersonalCabinet(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new PersonalCabinetPage());
+        }
+
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GoToMainPage(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckUsers(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckDisciplines(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckAllTasks(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckAllTaskBases(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ WpfApp2/Views/PersonalCabinetPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp2.Model;
+
+namespace WpfApp2.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для PersonalCabinetPage.xaml
+    /// </summary>
+    public partial class PersonalCabinetPage : Page
+    {
+        private double PagesCount;
+        private int NumberOfPage = 0;
+        private int maxItemShow = 5;
+        public PersonalCabinetPage()
+        {
+            InitializeComponent();
+            var currentServices = AppData.db.Event.ToList();
+            PagesCount = Math.Ceiling(Convert.ToDouble(currentServices.Count) / Convert.ToDouble(maxItemShow));
+            LViewTours.ItemsSource = currentServices.Skip(maxItemShow * NumberOfPage).Take(maxItemShow).ToList();
+        }
+
+        private void LBMyTasks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListViewItem_LeftMouseButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListViewItem).DataContext as Event;
+            if (item != null)
+            {
+                NavigationService.Navigate(new EventShowDetailsPage(item));
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
